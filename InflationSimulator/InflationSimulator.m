b66511c111812ce61f1c211857e192296e879e9f
Maksim Piskunov <maxitg@gmail.com>
2019-02-05 17:09:25 -0600

    Field density and pressure (#2)
    ## Changes
    
    * Add implementation of `FieldDensity` and `FieldPressure`.
    * Add proof of the equations used in the reference page for `FieldDensity`.
    
    ## Tests
    
    * Open documentation pages for `FieldDensity` and `FieldPressure` and evaluate examples.

--- InflationSimulator/InflationSimulator.m
+++ InflationSimulator/InflationSimulator.m
@@ -15,7 +15,7 @@
 BeginPackage["InflationSimulator`", {"UsageString`"}]
 
 
-InflationSimulator`$PublicSymbols = {};
+InflationSimulator`$PublicSymbols = {FieldDensity, FieldPressure};
 
 
 Unprotect @@ InflationSimulator`Private`$PublicSymbols;
@@ -27,6 +27,35 @@ ClearAll @@ InflationSimulator`Private`$PublicSymbols;
 
 
 Begin["`Private`"];
+
+
+(* ::Subsection:: *)
+(*Field Density*)
+
+
+FieldDensity::usage = UsageString[
+	"FieldDensity[`\[ScriptCapitalL]`, \!\(\*SubscriptBox[\(`\[CurlyPhi]`\), \(`t`\)]\)] yields the density ",
+	"of the field with Lagrangian `\[ScriptCapitalL]` and time derivative of the field ",
+	"\!\(\*SubscriptBox[\(`\[CurlyPhi]`\), \(`t`\)]\)."
+];
+
+
+FieldDensity[lagrangian_, fieldDerivative_] :=
+	-lagrangian + D[lagrangian, fieldDerivative] fieldDerivative
+
+
+(* ::Subsection:: *)
+(*Field Pressure*)
+
+
+FieldPressure::usage = UsageString[
+	"FieldPressure[`\[ScriptCapitalL]`, \!\(\*SubscriptBox[\(`\[CurlyPhi]`\), \(`t`\)]\)] yields the pressure ",
+	"of the field with Lagrangian `\[ScriptCapitalL]` and time derivative of the field ",
+	"\!\(\*SubscriptBox[\(`\[CurlyPhi]`\), \(`t`\)]\)."
+];
+
+
+FieldPressure[lagrangian_, fieldDerivative_] := lagrangian
 
 
 (* ::Section:: *)
