e00ff461937698bec919e44ede8d5f7686cf069c
Maksim Piskunov <maxitg@icloud.com>
2019-04-26 16:28:07 -0500

    Update version to 0.2.3.

--- PacletInfo.m
+++ PacletInfo.m
@@ -2,7 +2,7 @@
 
 Paclet[
     Name -> "InflationSimulator",
-    Version -> "0.2.2",
+    Version -> "0.2.3",
     MathematicaVersion -> "12.0+",
     Description -> "Code for simulating inflation, including Lagrangians with non-canonical kinetic energy.",
     Creator -> "Maksim Piskunov",
