0e79a8eb6b9dc746df2edd79ba2e36fe5b45a3a2
Maksim Piskunov <maxitg@icloud.com>
2019-05-25 16:03:50 -0500

    Use constraints from Planck 2018 results (#22)
    ## Changes
    
    * Implement Planck 2018 constraints on tensor-to-scalar ratio and scalar spectral index.
    
    ## Commands and tests
    
    * Evaluate new documentation page for `ExperimentallyConsistentInflationQ`. In particular, note that natural inflation example does no longer pass experimental constraints.

--- PacletInfo.m
+++ PacletInfo.m
@@ -2,7 +2,7 @@
 
 Paclet[
     Name -> "InflationSimulator",
-    Version -> "0.3.0",
+    Version -> "0.3.1",
     MathematicaVersion -> "12.0+",
     Description -> "Code for simulating inflation, including multiple field Lagrangians with non-canonical kinetic energy.",
     Creator -> "Maksim Piskunov",
